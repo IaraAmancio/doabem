08e58c77642bd7d0b9c7d3bba2ecbc90902bcb44
unknown <iaraamancio1986@gmail.com>
2026-04-28 21:06:01 -0300

    adicionando validacao de instituicao

---
+++ src/middlewares/validacao/instituicao.js
@@ -0,0 +1,41 @@
+
+const Joi = require("joi");
+
+const instituicaoSchema = Joi.object({
+    nome: Joi.string().min(2).required().messages({
+        "string.empty": "Nome da Instiuicao é obrigatório",
+        "string.min": "Monome Instituição deve ter no minímo 2 caracteres",
+        "any.require": "Nome da Instituição é obrigatório"
+    }),
+    email: Joi.string().email().required().messages({
+       "string.empty": "E-mail é obrigatório",
+        "string.email": "E-mail deve ser um e-mail válido",
+        "any.require": "E-mail é obrigatório" 
+    }),
+    senha: Joi.string().min(6).required().messages({
+        "string.base": "Senha deve ser string",
+        "string.empty": "Senha é obrigatória",
+        "string.min": "Senha deve ter no minímo 6 caracteres",
+        "any.require": "Senha é obrigatória"
+    }),
+    telefone: Joi.string().pattern(/^[0-9]{10,11}$/).required().messages({
+      'string.pattern.base': 'Telefone inválido. Use apenas números, incluindo o DDD.',
+      'any.required': 'Telefone é obrigatório'
+    }),
+});
+
+function validarInstituicao(req, res, next){
+    const {error} = instituicaoSchema.validate(req.body, {abortEarly:false});
+    if(error){
+        console.log(error);
+        return res.status(400).json({
+            erro: error.details.map(e => e.message)
+        })
+    };
+
+    next();
+
+}
+
+module.exports = validarInstituicao;
+
--- src/routes/instituicaoRoutes.js
+++ src/routes/instituicaoRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/instituicaoController");
+const validarInstituicao = require("../middlewares/validacao/instituicao");
 
-router.post("/instituicao", controller.criarInstituicao);
+
+router.post("/instituicao", validarInstituicao, controller.criarInstituicao);
 router.get("/instituicao", controller.listarInstituicoes);
 
 module.exports = router;
